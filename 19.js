cebc035d3af726162eaa36609f1d044bba1ce8a2
sonuth4 <162896114+sonuth4@users.noreply.github.com>
2024-12-29 15:01:15 +0545

    Delete 19.js

--- 19.js
+++
@@ -1,9 +0,0 @@
-const prompt = require('prompt-sync')({ sigint: true });
-function dateDifference() {
-    let date1 = new Date(prompt("Enter the first date (YYYY-MM-DD):"));
-    let date2 = new Date(prompt("Enter the second date (YYYY-MM-DD):"));
-    let diffInTime = Math.abs(date2 - date1);
-    let diffInDays = diffInTime / (1000 * 60 * 60 * 24);
-    console.log(`Difference in days: ${diffInDays}`);
-}
-dateDifference();
